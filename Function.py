a193a0aadc2a2eb0a0f49bf6ea8fb1855b1db982
Yuxin <76422684+Lyxxx2003@users.noreply.github.com>
2022-09-04 12:03:22 -0500

    Add another function

--- Function.py
+++ Function.py
@@ -3,3 +3,15 @@ def function(a, b):
     return c
 
 print(function(1, 2))
+
+Flag = True
+def advertising(array, int):
+    for i in array:
+        if i == int:
+            Flag = False
+    if Flag == False:
+        print("Found!")
+    else:
+        print("Not Found!")
+        
+advertising([1, 2, 3], 3)
